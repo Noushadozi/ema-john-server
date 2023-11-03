b5b19490244e472c00818d613ff88ff16f725329
Noushadozi <noushadozi333@gmail.com>
2023-11-03 22:42:32 +0600

    data loaded with query

--- index.js
+++ index.js
@@ -28,7 +28,13 @@ async function run() {
     const productCollection = client.db("ema-john").collection("products");
 
     app.get("/products", async (req, res) => {
-      const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const result = await productCollection
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
       res.send(result);
     });
 
